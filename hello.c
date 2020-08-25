2ae41668352d3ab7c00414ca172a710d707ae36c
jhaag3 <54606512+jhaag3@users.noreply.github.com>
2020-08-24 22:13:01 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jonathan Haag
+ *         jhaag5@huskers.unl.edu
+ * Date: 2020/08/18
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jonathan Haag!\n");
+
+  return 0;
+}
